fc5f5d39e335beb0ebbdc85cd7a3629b9a28a427
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2023-07-11 14:02:40 -0700

    pleasefix

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,20 +5,12 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
-<<<<<<< HEAD
-      User.hasMany(models.Booking, { foreignKey: 'userId' });
-      User.hasMany(models.Review, { foreignKey: 'userId' });
-      User.hasMany(models.Spot, { foreignKey: 'ownerId' });
-      User.belongsToMany(models.Spot, { through: models.Review, foreignKey: "userId", otherKey: "spotId" });
-      User.belongsToMany(models.Spot, { through: models.Booking, foreignKey: "userId", otherKey: "spotId" });
-=======
       User.hasMany(models.Booking, { foreignKey: 'userId', onDelete: 'CASCADE',
       hooks: true });
       User.hasMany(models.Review, { foreignKey: 'userId', onDelete: 'CASCADE',
       hooks: true });
       User.hasMany(models.Spot, { foreignKey: 'ownerId', onDelete: 'CASCADE',
       hooks: true });
->>>>>>> dev
     }
   };
 
