31133a61c36e454d0500ec753202771956321e46
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2023-07-14 13:00:49 -0700

    rendertest3

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -340,41 +340,8 @@ router.post('/:spotId/reviews', requireAuth, validateReview, async (req, res) =>
 
 //Get all Bookings for a Spot based on the Spot's id
 router.get('/:spotId/bookings', requireAuth, async (req, res) => {
-    if(req.user.id === req.spot.ownerId) {
-    const options = {
-        include: [
-            {
-                model: User,
-                attributes: ['id', 'firstName', 'lastName']
-            },
-            {
-                model: ReviewImage,
-                attributes: ['id', 'url']
-            },
-        ],
-        where: { spotId: req.params.spotId }
-    };
-    const reviews = await Review.findAll(options);
 
-    // Check if any reviews are found
-    if (reviews.length === 0) {
-        return res.status(404).json({ message: 'Spot not found' });
-    }
-
-    for (let i = 0; i < reviews.length; i++) {
-        const review = reviews[i].toJSON();
-        if (review.Spot) {
-            reviews[i] = review;
-            review.Spot.previewImage = review.Spot.SpotImages[0].url;
-            delete review.Spot.SpotImages;
-        }
-    }
-
-    res.json({ Reviews: reviews });
-} else if {
-    
-}
-});
+})
 
 
 
