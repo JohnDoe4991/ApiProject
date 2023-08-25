e25c9d0d0f6b8ab7bd83cd388b681afc94bb6a02
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2023-08-25 11:02:31 -0700

    details css

--- frontend/src/components/Spots/SpotDetails.js
+++ frontend/src/components/Spots/SpotDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetailsThunk } from "../../store/spots";
+import "./Spots.css/SpotDetails.css"
 
 
 export default function SpotDetails() {
@@ -26,31 +27,38 @@ export default function SpotDetails() {
 
     return (
         <>
-            <div className="name+city">
-                <h1>{thisSpot.name}</h1>
-                <h2>{thisSpot.city}, {thisSpot.state}, {thisSpot.country}</h2>
-            </div>
-            <div className="photos">
-                <div className="preview">
-                    <img src={thisSpot.SpotImages[0].url} alt="Preview" />
+            <div className="detail-container">
+                <div className="name-city">
+                    <h1>{thisSpot.name}</h1>
+                    <h2>{thisSpot.city}, {thisSpot.state}, {thisSpot.country}</h2>
                 </div>
-                {thisSpot.SpotImages.slice(1, 5).map((image, index) => (
-                    <div className="other-photos" key={index}>
-                        <img src={image.url} alt={`Image ${index}`} />
+                <div className="photos">
+                    <div className="preview">
+                        <img src={thisSpot.SpotImages[0].url} alt="Preview" />
                     </div>
-                ))}
-            </div>
-            <div className="footer">
-                <div className="hosted-by">
-                    <h3>Hosted by {thisSpot.User.firstName}{" "}{thisSpot.User.lastName}</h3>
+                        {thisSpot.SpotImages.slice(1, 5).map((image, index) => (
+                            <div className="other-photos" key={index}>
+                                <img src={image.url} alt={`Image ${index}`} />
+                            </div>
+                    ))}
                 </div>
-                <p>{thisSpot.description}</p>
-                <div className="pay-me-container">
-                    
-                    <h3>{thisSpot.price}</h3>
+                <div className="footer">
+                    <div className="hosted-by">
+                        <h3>Hosted by {thisSpot.User.firstName}{" "}{thisSpot.User.lastName}</h3>
+                    </div>
+                    <p>{thisSpot.description}</p>
+                    <div className="pay-me-container">
+                        <h3>${thisSpot.price}{" "}Night</h3>
+                        <div className="rating-reviews">
+                            {thisSpot.avgStarRating > 0 && <p className="rating">⭐️{thisSpot.avgStarRating.toFixed(1)}</p>}
+                            {!thisSpot.avgStarRating && <p className="new">⭐️New</p>}
+                            {" "}
+                            {thisSpot.numReviews} Reviews
+                        </div>
+                        <button className="reserve-button">Reserve</button>
+                    </div>
                 </div>
             </div>
-
         </>
     );
 }
