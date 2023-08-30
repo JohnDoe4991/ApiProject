401ed478e84c162c8fb731b53dec2727e9f0a09b
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2023-08-30 15:52:17 -0700

    sherry save me

--- frontend/src/components/Spots/SpotForms/CreateSpotForm/CreatSpotForm.js
+++ frontend/src/components/Spots/SpotForms/CreateSpotForm/CreatSpotForm.js
@@ -6,25 +6,5 @@ export default function CreateSpotForm() {
         <SpotForm
             formType="Create"
         />
-
-import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-
-
-export default function CreateSpotForm({ spots }) {
-    const history = useHistory()
-
-
-    return (
-        <>
-        <h1>Create a new Spot</h1>
-        <h2>Where's your place located?</h2>
-        <p>Guests will only get your exact address once they have booked a reservation.</p>
-            <form
-                className="create-form">
-
-            </form>
-        </>
-
     )
 }
--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -5,11 +5,13 @@ import { csrfFetch } from "./csrf";
 //types
 
 const GET_ALL_REVIEWS = "reviews/GET_REVIEWS"
+const POST_REVIEW = "reviews/POST_REVIEW"
 
 
 //action creator
 
 const actionGetReview = (reviews) => ({ type: GET_ALL_REVIEWS, reviews });
+const actionCreateReview = (review) => ({ type: POST_REVIEW, review })
 
 
 //Thunks
@@ -30,6 +32,19 @@ export const getAllReviewsThunk = (spotId) => async (dispatch) => {
     }
 };
 
+export const createReview = (spotId, review, stars) => async (dispatch) => {
+    const req = await csrfFetch(`/api/spots/${spotId}/reviews`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ review, stars })
+    });
+    const data = await req.json();
+    dispatch(actionCreateReview(data.review));
+    return data;
+};
+
 
 // normalizeArr
 
@@ -42,9 +57,15 @@ export default function reviewReducer(state = initialState, action) {
     let newState;
     switch (action.type) {
         case GET_ALL_REVIEWS:
-            newState = { ...state, reviews: { spot: {}, user: {} }  }
+            newState = { ...state, reviews: { spot: {}, user: {} } }
             newState.reviews.spot = action.reviews
             return newState;
+        case POST_REVIEW:
+            console.log("STATE before, ", state)
+            newState = { ...state, reviews: { spot: {}, user: {} } };
+            newState.reviews.spot = action.review
+            console.log("STATE after, ", state)
+            return newState
         default:
             return state;
     }
